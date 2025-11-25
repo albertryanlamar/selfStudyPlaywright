c13397b4a35b8643ebd027c20e2c3886011b4451
albertryanlamar@gmail.com <albertryanlamar@gmail.com>
2025-11-25 09:32:35 +0800

    Remove exposed GitHub token

--- tests/apiTest/apiTest_04_authentication.spec.ts
+++ tests/apiTest/apiTest_04_authentication.spec.ts
@@ -3,6 +3,10 @@ import method from '../../src/utils/apiMethods'
 import { envConfig } from '../../src/config/environment';
 import validations from '../../src/utils/validations';
 import { assert } from 'console';
+import dotenv from "dotenv";
+
+dotenv.config();
+export const token = process.env.GITHUB_TOKEN;
 
 
 test.beforeAll(async()=>{
@@ -29,7 +33,7 @@ test('Basic Auth',async ()=>{
 }) */
 
 test('Bearer Token',async ()=>{
-   // const token= "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";
+   
     const res = await method.gets(`https://api.github.com/user/repos`,{
         headers:{
                 Authorization:`Bearer ${token}`
